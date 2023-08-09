e18107bfb08cd0f18d2184880cabb65882ce0525
ztkyaojiayou <1727245876@qq.com>
2023-08-09 11:07:33 +0800

    测试模块init

---
+++ tkzou-spring/src/test/java/com/tkzou/middleware/test/HelloSpringTests.java
@@ -0,0 +1,15 @@
+package com.tkzou.middleware.test;
+
+import org.junit.Test;
+
+/**
+ * 测试类和对应的方法都必须是public，否则不会被Junit发现，会报错！！！
+ */
+public class HelloSpringTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("hello，spring！");
+    }
+
+}
