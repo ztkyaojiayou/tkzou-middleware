e275706714f111dfe878c686d927804acc455d50
ztkyaojiayou <1727245876@qq.com>
2023-08-09 10:53:06 +0800

    手写中间件项目init

---
+++ tkzou-spring/src/main/java/com/tkzou/middleware/spring/TkzouSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tkzou.middleware.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkzouSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TkzouSpringApplication.class, args);
+    }
+
+}
