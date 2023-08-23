aeff8ebe43a3602da48f82b274d4942d97b9bd39
tkzou <tongkunzou0807@163.com>
2023-08-23 23:32:02 +0800

    在xml⽂件中定义bean--03调整BeanFactory的继承层次，尽量与spring保持一致，也方便后续功能开发

---
+++ tkzou-spring/src/main/java/com/tkzou/middleware/spring/beans/factory/config/AutowireCapableBeanFactory.java
@@ -0,0 +1,14 @@
+package com.tkzou.middleware.spring.beans.factory.config;
+
+import com.tkzou.middleware.spring.beans.factory.BeanFactory;
+
+/**
+ * 不知道是干嘛，先写着！
+ * 原来是为了⽅便后⾯的讲解和功能实现，并且尽量保持和spring中BeanFactory的继承层次⼀致，
+ * 对BeanFactory的继承层次稍微做了调整。
+ * 先就这么着！
+ *
+ * @author zoutongkun
+ */
+public interface AutowireCapableBeanFactory extends BeanFactory {
+}
---
+++ tkzou-spring/src/main/java/com/tkzou/middleware/spring/beans/factory/config/ConfigurableBeanFactory.java
@@ -0,0 +1,10 @@
+package com.tkzou.middleware.spring.beans.factory.config;
+
+import com.tkzou.middleware.spring.beans.factory.HierarchicalBeanFactory;
+
+/**
+ * 不知道是干嘛，先写着！
+ * @author zoutongkun
+ */
+public interface ConfigurableBeanFactory extends HierarchicalBeanFactory,SingletonBeanRegistry {
+}
