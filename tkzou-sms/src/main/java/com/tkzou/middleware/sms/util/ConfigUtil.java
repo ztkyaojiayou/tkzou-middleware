e299b70f73aa30af214fe776c54cdeed708830ad
tkzou <1727245876@qq.com>
2024-09-22 16:55:07 +0800

    tkzou-sms：ConfigUtil整合

--- tkzou-sms/src/main/java/com/tkzou/middleware/sms/util/ConfigUtil.java
+++ tkzou-sms/src/main/java/com/tkzou/middleware/sms/util/ConfigUtil.java
@@ -1,7 +1,10 @@
 package com.tkzou.middleware.sms.util;
 
+import cn.hutool.core.io.file.FileReader;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 /**
  * ConfigUtil
@@ -25,5 +28,16 @@ public class ConfigUtil {
 
     public <T> T getValue(String key, Class<T> type) {
         return environment.getProperty(key, type);
+    }
+
+    /**
+     * 读取配置文件
+     */
+    public static String getConfig(String path) {
+        return new FileReader(path).readString();
+    }
+
+    public static String getConfig() {
+        return getConfig(Objects.requireNonNull(ConfigUtil.class.getResource("/smsConfig.json")).getPath());
     }
 }
